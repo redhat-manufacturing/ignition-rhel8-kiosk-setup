3cdd686454aee4da37ddb40505f819375ad77763
kelee <kelee@localhost.localdomain>
2021-10-15 02:45:41 -0500

    update setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,17 @@
+# install required packages
+
+dnf -y install podman git
+
 # create the kiosk ignition user
+
 useradd -p $(openssl passwd -1 password) ignition-kiosk-user
 
 # add kiosk ignition user to sudoers
+
 echo -e "ignition-kiosk-user\tALL=(ALL)\tNOPASSWD: ALL" > /etc/sudoers.d/020_ignition-kiosk-user
 
 # enable autologin for the kiosk ignition user
+
 cp /etc/gdm/custom.conf /etc/gdm/custom.conf.bak
 cat <<EOF >/etc/gdm/custom.conf
 # GDM configuration storage
@@ -16,6 +23,27 @@ cat <<EOF >/etc/gdm/custom.conf
 AutomaticLoginEnable=true
 # This is the user that should be logged in immediately if AutomaticLoginEnable is true.
 AutomaticLogin=ignition-kiosk-user
-
 EOF
+
+# create startup routine for ignition-kios-user
+
+mkdir -p /home/ignition-kiosk-user/.config/autostart
+cat <<EOF >/home/ignition-kiosk-user/.config/autostart/ignition-kiosk-script.desktop
+[Desktop Entry]
+Type=Application
+Exec=/home/ignition-kiosk-user/ignition-rhel8-kiosk-setup/scripts/run.sh
+Hidden=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=ignition-kiosk
+name=ignition-kiosk
+EOF
+
+# clone repository
+
+git clone https://github.com/keunlee/ignition-rhel8-kiosk-setup.git /home/ignition-kiosk-user/
+
+# change file/dir owner to ignition-kiosk-user
+
+chown -R ignition-kiosk-user /home/ignition-kiosk-user/ignition-rhel8-kiosk-setup
+chown -R ignition-kiosk-user /home/ignition-kiosk-user/.config
 
