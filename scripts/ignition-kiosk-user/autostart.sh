29da33b8e94e984aa5e134a7fdc764a5e3106b0a
keunlee <kelee@redhat.com>
2021-10-15 11:28:56 -0500

    update autostart logic

--- scripts/ignition-kiosk-user/autostart.sh
+++ scripts/ignition-kiosk-user/autostart.sh
@@ -3,12 +3,19 @@ IGNITION_LICENSE_KEY=$(</home/ignition-kiosk-user/ignition-rhel8-kiosk-setup/scr
 IGNITION_ACTIVATION_TOKEN_FILE_PATH=/home/ignition-kiosk-user/ignition-rhel8-kiosk-setup/scripts/ignition-kiosk-user/activation-token
 IGNITION_CLIENT_URL=http://localhost:8088/data/perspective/client/samplequickstart
 
-podman run -p 8088:8088 --name my-ignition-maker --privileged \
--e GATEWAY_ADMIN_PASSWORD=password \
--e IGNITION_EDITION=maker \
--e IGNITION_LICENSE_KEY=$IGNITION_LICENSE_KEY \
--v $IGNITION_ACTIVATION_TOKEN_FILE_PATH:/activation-token \
--e IGNITION_ACTIVATION_TOKEN_FILE=/activation-token \
--d quay.io/kelee/ignition-maker-ubi:8.1.10
+CONTAINER_EXISTS=$(podman container exists my-ignition-maker; echo $?)
 
-firefox -kiosk $IGNITION_CLIENT_URL
+if [[ CONTAINER_EXISTS -eq 0 ]]
+then
+    podman start my-ignition-maker
+    sleep 30
+    firefox -kiosk $IGNITION_CLIENT_URL
+else
+    podman run -p 8088:8088 --name my-ignition-maker --privileged \
+    -e GATEWAY_ADMIN_PASSWORD=password \
+    -e IGNITION_EDITION=maker \
+    -e IGNITION_LICENSE_KEY=$IGNITION_LICENSE_KEY \
+    -v $IGNITION_ACTIVATION_TOKEN_FILE_PATH:/activation-token \
+    -e IGNITION_ACTIVATION_TOKEN_FILE=/activation-token \
+    -d quay.io/kelee/ignition-maker-ubi:8.1.10
+fi
