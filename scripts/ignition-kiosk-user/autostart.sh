2e89509cf692aab133c14b8edf76eae6d016ae55
keunlee <kelee@redhat.com>
2021-10-15 10:56:37 -0500

    update autostart to start podman and open kiosk app

--- scripts/ignition-kiosk-user/autostart.sh
+++ scripts/ignition-kiosk-user/autostart.sh
@@ -1,6 +1,7 @@
 # start podman
 IGNITION_LICENSE_KEY=$(</home/ignition-kiosk-user/ignition-rhel8-kiosk-setup/scripts/ignition-kiosk-user/license-key)
 IGNITION_ACTIVATION_TOKEN_FILE_PATH=/home/ignition-kiosk-user/ignition-rhel8-kiosk-setup/scripts/ignition-kiosk-user/activation-token
+IGNITION_CLIENT_URL=http://localhost:8088/data/perspective/client/samplequickstart
 
 podman run -p 8088:8088 --name my-ignition-maker --privileged \
 -e GATEWAY_ADMIN_PASSWORD=password \
@@ -9,3 +10,5 @@ podman run -p 8088:8088 --name my-ignition-maker --privileged \
 -v $IGNITION_ACTIVATION_TOKEN_FILE_PATH:/activation-token \
 -e IGNITION_ACTIVATION_TOKEN_FILE=/activation-token \
 -d quay.io/kelee/ignition-maker-ubi:8.1.10
+
+firefox -kiosk $IGNITION_CLIENT_URL
